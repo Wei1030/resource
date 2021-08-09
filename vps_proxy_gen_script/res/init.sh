104d81541e093988b912888a56b1db30acae1090
Wei1030 <maowei208@126.com>
2021-08-10 01:35:32 +0800

    rep

--- vps_proxy_gen_script/res/init.sh
+++ vps_proxy_gen_script/res/init.sh
@@ -3,4 +3,11 @@
 
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
+curl -O https://raw.githubusercontent.com/Wei1030/resource/main/vps_proxy_gen_script/res/config.json
+
+cp config.json /usr/local/etc/v2ray/config.json -f
+
 systemctl stop firewalld
+
+systemctl start v2ray
+systemctl enable v2ray
