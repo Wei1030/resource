49a1b7dd87e059da0f5462c880afa9a71d615756
Wei1030 <40414769+Wei1030@users.noreply.github.com>
2021-08-28 18:07:17 +0800

    Update init.sh

--- vps_proxy_gen_script/res/init.sh
+++ vps_proxy_gen_script/res/init.sh
@@ -4,8 +4,12 @@
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
 curl -O https://raw.githubusercontent.com/Wei1030/resource/main/vps_proxy_gen_script/res/config.json
+curl -O https://raw.githubusercontent.com/Wei1030/resource/main/vps_proxy_gen_script/res/server.crt
+curl -O https://raw.githubusercontent.com/Wei1030/resource/main/vps_proxy_gen_script/res/server.key
 
 \cp -f config.json /usr/local/etc/v2ray/config.json
+\cp -f config.json /usr/local/etc/v2ray/server.crt
+\cp -f config.json /usr/local/etc/v2ray/server.key
 
 systemctl stop firewalld
 
